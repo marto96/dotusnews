6aac51cb5ebc4618ddae5459a8a1e311165ee20b
marto96 <marto9610@hotmail.com>
2019-08-12 22:38:53 -0500

    email

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
+
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +10,25 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
-  constructor() {}
+  myForm: FormGroup;
+  constructor(
+    public formBuilder: FormBuilder
+  ) {
+    this.myForm = this.createMyForm();
+  }
+  segmentChanged(ev: any) {
+    console.log('Segment changed', ev);
+  }
+  saveData() {
+    console.log(this.myForm.value);
+  }
+  private createMyForm() {
+    return this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      gender: ['', Validators.required],
+    });
+}
 
 }
+
